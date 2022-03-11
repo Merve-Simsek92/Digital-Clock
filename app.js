9ce418217b39d022af2e6a222f464ab0d26a9d35
ucurtmaavcısı <simsekmerve1717@gmail.com>
2022-03-11 17:29:49 +0300

    çalışma

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-function showTime(){
-    var date=new Date();
-    var h=date.getHours();
-    var m=date.getMinutes();
-    var s=date.getSeconds();
-
-h=h<10 ? "0"+ h : h;
-m=m<10 ? "0"+ m : m;
-s=s<10 ? "0"+ s : s;
-
-
-let time=document.querySelector(".clock");
-time.innerText=`${h}:${m}:${s}`
-
-
-
-setInterval(showTime,1000)
- }
- showTime()
 // function showTime(){
 //     var date=new Date();
-//     var time=date.toLocaleTimeString();
-//    let t=document.querySelector(".clock");
-//    t.innerText=time;
+//     var h=date.getHours();
+//     var m=date.getMinutes();
+//     var s=date.getSeconds();
+//      var x="AM";
+ 
+// if(h == 0){
+//     h = 12;
 // }
+
+// if(h > 12){
+//     h = h - 12
+//     x="PM"}
+
+
+// h=h<10 ? "0"+ h : h;
+// m=m<10 ? "0"+ m : m;
+// s=s<10 ? "0"+ s : s;
+
+
+// let time=document.querySelector(".clock");
+// time.innerText=`${h}:${m}:${s}  ${x}`
+
+
+
 // setInterval(showTime,1000)
-// showTime()
+//  }
+//  showTime()
+function showTime(){
+    var date=new Date();
+    var time=date.toLocaleTimeString();
+   let t=document.querySelector(".clock");
+   t.innerText=time;
+}
+setInterval(showTime,1000)
+showTime()
